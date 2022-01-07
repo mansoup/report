d4c98d85dc2c0ed376c0204309fb3a6295babd61
mansoup <18379671061@163.com>
2022-01-07 10:15:54 +0800

    first commit

---
+++ doc/docs/guide/question.md
@@ -0,0 +1,54 @@
+- 使用Maven Install打包时报错 <br>
+  ![img.png](../picture/qusetion/img.png) <br>
+  请使用Maven Package进行打包 <br>
+
+### 版本问题：[开发环境参考](https://report.anji-plus.com/report-doc/guide/quicklyDevelop.html)
+
+- 底层数据库为Mysql8.0+时，flyway执行SQL报错<br>
+- Node.js是V16版本时 npm install失败 <br>
+- jdk使用1.7或者11及以上时，编译打包不过 <br>
+  ![img](../picture/qusetion/img_1.png) <br>
+
+- MssSqlServer 2014及其上下版本数据源测试不过。以2014版本为例。 <br>
+  ![img](../picture/qusetion/img_2.png) <br>
+  将1.2.6改成1.2.0 <br>
+
+- 浏览器兼容性 当前未对部分浏览器做兼容性适配，推荐使用谷歌浏览器进行访问。<br>
+
+- 部署完进入系统，点击预览大屏，大屏提示：“执行sql失败“ <br>
+  ![img](../picture/qusetion/img_3.png) <br>
+  请先重置mysql数据源，将mysql数据源的账号密码修改为你当前系统的账号密码。<br>
+
+- flyway执行报错1.0.10sql失败 <br>
+  错误提示：Caused by: org.flywaydb.core.api.FlywayException: Validate failed: Detected failed <br>
+  migration to version 1.0.10 (create report share) <br>
+  请参考此 [Issue](https://gitee.com/anji-plus/report/issues/I47JNE) 解决此问题 <br>
+
+- 禁用flyway及切换底层数据库方案 <br>
+  [链接](https://my.oschina.net/u/4517014/blog/5269319) <br>
+
+- 数据源数据集用法总结 <br>
+  [链接](https://my.oschina.net/u/4517014/blog/5270828) <br>
+  **注**：http数据源未来会挪到数据集那边 <br>
+
+### 执行源码编译脚本（build.sh）报错
+
+- 提示：“*** report-ui/dist/* *** No such file or directory” <br>
+  前端编译失败。<br>
+  99%的原因是Node.js版本过高（高于14），导致前端编译失败，Nodejs在编译执行初始化时会去下载一些依赖，如果依赖下载不来，也会导致失败。<br>
+  剩下极少数情况可能是你编译的linux系统问题。<br>
+
+- 提示：“report-core/target/aj-report-*.zip *** No such file or directory” <br>
+  后端编译失败。<br>
+  可能原因有：Mvn版本过低/过高，导致后端编译失败 <br>
+
+- 使用eclipse进行源码编译时失败 <br>
+  失败的提示有很多，这里建议换成IDEA  <br>
+
+- 使用IDEA进行源码编译时提示：“*** openjdk-***” <br>
+  请使用jdk1.8
+
+
+
+
+
---
+++ report-ui/src/store/modules/user.js
@@ -0,0 +1,77 @@
+import { login, logout } from '@/api/login'
+import {setToken, delToken, setAccessUser, delAccessUser, delShareToken} from '@/utils/auth'
+//@soup 2022/1/5 增加注释 下面这个是vuex
+const user = {
+  state: {
+    token: '', // 登录后的token
+    accessUser: {} // 登录后的用户对象
+  },
+
+  mutations: {
+    SET_TOKEN: (state, token) => {
+      state.token = token
+      setToken(token)
+    },
+    SET_ACCESSUSER: (state, accessUser) => {
+      state.accessUser = accessUser
+      setAccessUser(accessUser)
+    }
+  },
+
+  actions: {
+    // 登录
+    Login({ commit }, userInfo) {
+      const userName = userInfo.userName.trim()
+      const password = userInfo.password.trim()
+      const captchaVerification = userInfo.captchaVerification.trim()
+      var data ={
+        userName, password,captchaVerification
+      }
+      return new Promise((resolve, reject) => {
+        login(data).then(response => {
+          const repCode = response.repCode
+          const repData = response.repData
+          if (repCode === '0000') {
+            commit('SET_TOKEN', repData.token)
+            commit('SET_ACCESSUSER', repData.accessUser)
+            resolve(response)
+          } else {
+            reject(response.repMsg)
+          }
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 登出
+    LogOut({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('SET_TOKEN', '')
+          commit('SET_ACCESSUSER', {})
+          delToken()
+          delShareToken()
+          delAccessUser()
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 前端 登出
+    FedLogOut({ commit }) {
+      return new Promise(resolve => {
+        commit('SET_TOKEN', '')
+        commit('SET_ACCESSUSER', {})
+        delToken()
+        delShareToken()
+        delAccessUser()
+        resolve()
+      })
+    }
+  }
+}
+
+export default user
